3293c794d64783415e831efe47b113300bc8f8f7
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2022-06-26 13:58:22 +0200

    Update script.js

--- Z1/script.js
+++ Z1/script.js
@@ -1,201 +1,116 @@
-function prepareObject() {
-    const form = document.getElementById("element-form");
-    const data = new FormData(form);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css"/>
+    <script src="script.js"></script>
+</head>
+<body>
 
-    const object = {};
-    for (let el of data.entries()) {
-        object[el[0]] = el[1];
-    }
+<div class="container">
+    <div class="column half-column">
 
-    return object;
-}
+        <h3>Formularz</h3>
 
-function addElement() {
-
-    const object = prepareObject();
-
-    const valid = validate(object);
-    if (!valid) {
-        return;
-    }
-
-    const newPre = document.createElement("pre");
-    newPre.className = "column half-column element";
-    newPre.innerText = JSON.stringify(object, null, 2);
-
-    document.getElementById("db").append(newPre);
-}
+        <form id="element-form">
+            <div class="row">
+                <div class="column label-column">
+                    <label>Imię</label>
+                </div>
+                <div class="column input-column">
+                    <input type="text" name="name"/>
+                </div>
+            </div>
 
 
-function validate(object) {
+            <div class="row">
+                <div class="column label-column">
+                    <label>Numer</label>
+                </div>
+                <div class="column input-column">
+                    <input type="number" name="number"/>
+                </div>
+            </div>
 
-    // walidujemy wszystkie pola
-    const validNumber = validateNumber(object.number);
-    const validRadio = validateRadio(object.favouriteNumber);
-    const validPassword = validatePassword(object.password);
-    const validRepeatedPassword = validateRepeatedPassword(object.password, object.password2);
+            <div class="row">
+                <div class="column label-column">
+                    <label>Hasło</label>
+                </div>
+                <div class="column input-column">
+                    <input type="password" name="password"/>
+                </div>
+            </div>
 
-    // zwracamy wynik walidacji - że formularz ją przeszedł, wszystkie pola muszą być wypełnione poprawnie
-    return validNumber && validRadio && validPassword && validRepeatedPassword;
-}
+            <div class="row">
+                <div class="column label-column">
+                    <label>Powtórz hasło</label>
+                </div>
+                <div class="column input-column">
+                    <input type="password" name="password2"/>
+                </div>
+            </div>
 
-function validateNumber(number) {
+            <div class="row">
+                <div class="column label-column">
+                    <label>Data</label>
+                </div>
+                <div class="column input-column">
+                    <input type="date" name="date" value="2020-11-06"/>
+                </div>
+            </div>
 
-    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy jest większy lub równy 0
-    const valid = number >= 0;
+            <div class="row">
+                <div class="column label-column">
+                    <label>Checkbox</label>
+                </div>
+                <div class="column input-column">
+                    <input type="checkbox" name="checkbox"/>
+                </div>
+            </div>
 
-    // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
-    const input = document.querySelector("input[name='number']");
-
-    if (valid) {
-        // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
-
-        // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
-        input.className = "";
-
-        const nameMessage = document.getElementById("number-input-message");
-        if (nameMessage) {
-            //jeśli wyświetla się komunikat - usuwamy go
-            nameMessage.parentElement.removeChild(nameMessage);
-        }
-    } else {
-        // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
-
-        // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
-        input.className = "invalid";
-
-        // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
-        if (!document.getElementById("number-input-message")) {
-            // tworzymy element, który będzie mówił o błędzie w wybranym polu
-            const small = document.createElement("small");
-            small.id = "number-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
-            small.className = "invalid"; // nadajemy klasę - żeby był czerwony
-            small.innerText = "Niepoprawny numer - dopuszczalna tylko dodatnia liczba"; // dodajemy tekst, który wyświetli się użytkownikowi
-
-            // doczepiamy element jako "rodzeństwo" inputa
-            input.parentElement.appendChild(small);
-        }
-    }
-
-    // zwracamy wynik walidacji
-    return valid;
-}
-
-function validateRadio(radio) {
-    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy jest większy lub równy 0
-        const valid = radio > 0;
-
-        // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
-        const input = document.querySelector("input[name='favouriteNumber']");
-
-        if (valid) {
-            // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
-
-            // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
-            input.className = "";
-
-            const nameMessage = document.getElementById("favouriteNumber-input-message");
-            if (nameMessage) {
-                //jeśli wyświetla się komunikat - usuwamy go
-                nameMessage.parentElement.removeChild(nameMessage);
-            }
-        } else {
-            // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
-
-            // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
-            input.className = "invalid";
-
-            // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
-            if (!document.getElementById("favouriteNumber-input-message")) {
-                // tworzymy element, który będzie mówił o błędzie w wybranym polu
-                const small = document.createElement("small");
-                small.id = "favouriteNumber-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
-                small.className = "invalid"; // nadajemy klasę - żeby był czerwony
-                small.innerText = "Zaznacz jedną opcję"; // dodajemy tekst, który wyświetli się użytkownikowi
-
-                // doczepiamy element jako "rodzeństwo" inputa
-                input.parentElement.appendChild(small);
-            }
-        }
-
-        // zwracamy wynik walidacji
-
-        return valid;
-
-}
-
-function validatePassword(password) {
-    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy jest większy lub równy 0
-        const valid = (password.length>7);
-
-        // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
-        const input = document.querySelector("input[name='password']");
-
-        if (valid) {
-            // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
-
-            // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
-            input.className = "";
-
-            const nameMessage = document.getElementById("password-input-message");
-            if (nameMessage) {
-                //jeśli wyświetla się komunikat - usuwamy go
-                nameMessage.parentElement.removeChild(nameMessage);
-            }
-        } else {
-            // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
-
-            // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
-            input.className = "invalid";
-
-            // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
-            if (!document.getElementById("password-input-message")) {
-                // tworzymy element, który będzie mówił o błędzie w wybranym polu
-                const small = document.createElement("small");
-                small.id = "password-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
-                small.className = "invalid"; // nadajemy klasę - żeby był czerwony
-                small.innerText = "Hasło powinno zawierać przynajmniej 8 znaków."; // dodajemy tekst, który wyświetli się użytkownikowi
-
-                // doczepiamy element jako "rodzeństwo" inputa
-                input.parentElement.appendChild(small);
-            }
-        }
-
-        // zwracamy wynik walidacji
-        return valid;
-}
-
-function validateRepeatedPassword(password, repeatedPassword) {
-
-            const valid = (password == repeatedPassword);
-
-            const input = document.querySelector("input[name='password2']");
-
-            if (valid) {
-
-                input.className = "";
-
-                const nameMessage = document.getElementById("password2-input-message");
-                if (nameMessage) {
-
-                    nameMessage.parentElement.removeChild(nameMessage);
-                }
-            } else {
-
-                input.className = "invalid";
+            <div class="row">
+                <div class="column label-column">
+                    <label>Radio</label>
+                </div>
+                <div class="column input-column">
+                    <input type="radio" name="favouriteNumber" value="1"><label class="radio-label">1</label>
+                    <input type="radio" name="favouriteNumber" value="2"><label class="radio-label">2</label>
+                    <input type="radio" name="favouriteNumber" value="3"><label class="radio-label">3</label>
+                </div>
+            </div>
 
 
-                if (!document.getElementById("password2-input-message")) {
+            <div class="row">
+                <div class="column label-column">
+                    <label>Zwierzę</label>
+                </div>
+                <div class="column input-column">
+                    <select name="favouritePet">
+                        <option value="cat">Kot</option>
+                        <option value="dog">Pies</option>
+                        <option value="fish">Ryba</option>
+                    </select>
+                </div>
+            </div>
 
-                    const small = document.createElement("small");
-                    small.id = "password2-input-message";
-                    small.className = "invalid";
-                    small.innerText = "Hasła różnią się.";
 
-                    input.parentElement.appendChild(small);
-                }
-            }
+            <div class="row">
+                <div class="pull-right">
+                    <button type="button" class="btn" onclick="addElement();">Dodaj</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
 
 
-    return valid;
-}
+    <div class="column half-column">
+        <h3>Baza danych</h3>
+
+        <div id="db"></div>
+
+    </div>
+</div>
+
+</body>
+</html>
