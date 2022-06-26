aabb69d19a72ed151358199ca970b74c90190393
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2022-06-26 13:52:55 +0200

    Update script.js

--- Z1/script.js
+++ Z1/script.js
@@ -82,19 +82,120 @@ function validateNumber(number) {
 }
 
 function validateRadio(radio) {
-    // todo
+    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy jest większy lub równy 0
+        const valid = radio > 0;
 
-    return true;
+        // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
+        const input = document.querySelector("input[name='favouriteNumber']");
+
+        if (valid) {
+            // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
+
+            // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
+            input.className = "";
+
+            const nameMessage = document.getElementById("favouriteNumber-input-message");
+            if (nameMessage) {
+                //jeśli wyświetla się komunikat - usuwamy go
+                nameMessage.parentElement.removeChild(nameMessage);
+            }
+        } else {
+            // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
+
+            // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
+            input.className = "invalid";
+
+            // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
+            if (!document.getElementById("favouriteNumber-input-message")) {
+                // tworzymy element, który będzie mówił o błędzie w wybranym polu
+                const small = document.createElement("small");
+                small.id = "favouriteNumber-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
+                small.className = "invalid"; // nadajemy klasę - żeby był czerwony
+                small.innerText = "Zaznacz jedną opcję"; // dodajemy tekst, który wyświetli się użytkownikowi
+
+                // doczepiamy element jako "rodzeństwo" inputa
+                input.parentElement.appendChild(small);
+            }
+        }
+
+        // zwracamy wynik walidacji
+
+        return valid;
+
 }
 
 function validatePassword(password) {
-    // todo
+    // walidujemy numer, podany w argumencie - w tym wypadku sprawdzamy, czy jest większy lub równy 0
+        const valid = (password.length>7);
 
-    return true;
+        // odszukujemy na stronie odpowiednie pole - input, w którym został wpisany numer
+        const input = document.querySelector("input[name='password']");
+
+        if (valid) {
+            // jeśli numer pasuje do wzorca - usuwamy ewentualne komunikaty walidacyjne, jeśli są
+
+            // ustawiamy pustą klasę - w razie jakby input był wcześniej oznaczony na czerwono
+            input.className = "";
+
+            const nameMessage = document.getElementById("password-input-message");
+            if (nameMessage) {
+                //jeśli wyświetla się komunikat - usuwamy go
+                nameMessage.parentElement.removeChild(nameMessage);
+            }
+        } else {
+            // numer nie pasuje do wzorca - dodajemy komunikaty walidacyjne, jeśli ich nie ma
+
+            // dodajemy inputowi klasę, która oznacza, że coś z nim nie tak
+            input.className = "invalid";
+
+            // sprawdzamy, czy wyświetla się komunikat o błędzie w polu z numerem
+            if (!document.getElementById("password-input-message")) {
+                // tworzymy element, który będzie mówił o błędzie w wybranym polu
+                const small = document.createElement("small");
+                small.id = "password-input-message"; // nadajemy id - potem dzięki niemu dostaniemy się do elementu, żeby go usunąć
+                small.className = "invalid"; // nadajemy klasę - żeby był czerwony
+                small.innerText = "Hasło powinno zawierać przynajmniej 8 znaków."; // dodajemy tekst, który wyświetli się użytkownikowi
+
+                // doczepiamy element jako "rodzeństwo" inputa
+                input.parentElement.appendChild(small);
+            }
+        }
+
+        // zwracamy wynik walidacji
+        return valid;
 }
 
 function validateRepeatedPassword(password, repeatedPassword) {
-    // todo
 
-    return true;
+            const valid = (password == repeatedPassword);
+
+            const input = document.querySelector("input[name='password2']");
+
+            if (valid) {
+
+                input.className = "";
+
+                const nameMessage = document.getElementById("password2-input-message");
+                if (nameMessage) {
+
+                    nameMessage.parentElement.removeChild(nameMessage);
+                }
+            } else {
+
+                input.className = "invalid";
+
+
+                if (!document.getElementById("password2-input-message")) {
+
+                    const small = document.createElement("small");
+                    small.id = "password2-input-message";
+                    small.className = "invalid";
+                    small.innerText = "Hasła różnią się.";
+
+                    input.parentElement.appendChild(small);
+                }
+            }
+
+
+    return valid;
 }
