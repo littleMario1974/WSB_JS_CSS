218682c7616fed9fae01e252ec953eef03e1a3d5
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2022-07-05 19:20:03 +0200

    Update script2.js

--- Z2/script2.js
+++ Z2/script2.js
@@ -4,9 +4,9 @@ function getResult() {
     const amount1 = document.getElementById("amount1").value;
     const amount2 = document.getElementById("amount2").value;
 
-    axios.get("https://api.exchangerate.host/convert?from=" + amount1 +"&to" + amount2).then(response => {
+    axios.get("https://api.exchangerate.host/convert?from=" + amount1 +"&to=" + amount2).then(response => {
 
-        const result = response.data.text;
+        const result = response.data.result;
         document.getElementById("result").innerText = result;
     });
 }
