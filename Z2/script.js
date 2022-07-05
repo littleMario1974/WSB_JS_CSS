7531e82cbf7d9f82f44d79a5e4d6c2213ee4f9ac
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2022-07-05 19:39:05 +0200

    Update script.js

--- Z2/script.js
+++ Z2/script.js
@@ -6,10 +6,12 @@ function getFact() {
 
 
     axios.get("https://cat-fact.herokuapp.com/facts/random?animal_type=" + animalType + "&amount=" + number).then(response => {
-        
-	//response.data.forEach(d => console.log(d.text));
-        
-	const fact = response.data.text;
-        document.getElementById("fact").innerText = fact;
+
+        //response.data.forEach(d => console.log(d.text));
+
+        const fact = response.data.text;
+        for (let i = 0; i < response.data.length; i++) {
+            document.getElementById("fact").innerText += response.data[i].text;
+        }
     });
 }
